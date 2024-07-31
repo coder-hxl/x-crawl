a714b38bcc34fdd563c5e1a4407f792400611102
coderhxl <coderhxl@outlook.com>
2024-07-31 22:40:55 +0800

    docs: sponsor text color

--- docs/.vitepress/theme/components/SponsorsAside.vue
+++ docs/.vitepress/theme/components/SponsorsAside.vue
@@ -12,47 +12,13 @@ const { frontmatter } = useData()
 </template>
 
 <style scoped>
-.sponsor-container {
-  --max-width: 100%;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(var(--max-width), 1fr));
-  column-gap: 4px;
-}
-
-.sponsor-item {
-  margin: 2px 0;
-  padding: 10px 6px;
+a.sponsors-aside-text {
+  color: rgba(60, 60, 60, 0.5);
   display: block;
-  border-radius: 2px;
-  color: rgba(60, 60, 60, 0.5);
-  background-color: #f6f6f6;
-}
-
-.sponsor-item-img {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-}
-
-.sponsor-item img {
-  max-width: calc(var(--max-width) - 30px);
-  max-height: calc(var(--max-width) / 2 - 20px);
-}
-.sponsor-item-description {
-  text-align: center;
-  font-size: 13px;
-  color: rgb(74, 74, 74);
-}
-
-.sponsor-item-action {
-  margin: 2px 0;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  border-radius: 2px;
-  height: 30px;
+  margin: 3em 0 1em;
+  font-weight: 700;
   font-size: 11px;
-  color: rgba(60, 60, 60, 0.5);
-  background-color: #f6f6f6;
+  text-transform: uppercase;
+  letter-spacing: 0.4px;
 }
 </style>
